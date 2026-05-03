d1242b63450d7a5ba85ca49cd7b0ce3fd3ceaed4
sbarer <simonbarer@gmail.com>
2026-05-03 19:21:21 +1000

    printing game api to debug failure2

--- games.py
+++ games.py
@@ -41,13 +41,13 @@ def get_games_from_nhl():
         date = (TODAY + timedelta(days=i)).isoformat()
         data = get_schedule(date)
 
-        print("games")
-        print(i, data)
+        # print(i, data)
 
         for day in data.get("gameWeek", []):
             for g in day.get("games", []):
-                print("game")
-                print(g)
+                if "startTimeUTC" not in g:
+                    print("🚨 Missing startTimeUTC:", g)
+
                 start = datetime.fromisoformat(g["startTimeUTC"].replace("Z", "+00:00"))
                 cutoff = datetime.combine(
                     TODAY + timedelta(days=LOOKAHEAD_DAYS),
